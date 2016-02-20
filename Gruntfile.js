abf41bf2d49dba03e61e3cad3436fe3c1188627c
Yuriy Hvozdovych <gvizdyk@gmail.com>
2016-02-20 04:53:47 +0200

    big site overhaul

---
+++ Gruntfile.js
@@ -0,0 +1,74 @@
+module.exports = function (grunt) {
+  'use strict';
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    less: {
+      dist: {
+        options: {
+          paths: [],
+          compress: true,
+          yuicompress: true,
+          optimization: 2,
+          cleancss: true
+        },
+        files: {"css/compiled.css": "less/bootstrap.less"}
+      }
+    },
+
+    postcss: {
+      options: {
+        processors: [
+          require('autoprefixer')({
+            browsers: ['last 2 versions'],
+          })
+        ]
+      },
+      dist: {
+        src: 'css/compiled.css'
+      }
+    },
+
+    concat: {
+      js: {
+        src: [
+          'js/flipclock.js',
+          'js/marathonlviv.js'
+        ],
+        dest: 'js/compiled.min.js'
+      }
+    },
+
+    uglify: {
+      options: {
+        banner: '/*! Site: <%= pkg.name %>; Version: <%= pkg.version %>; Author: <%= pkg.author %>; Last modification: <%= grunt.template.today("yyyy-mm-dd")%>. */\n'
+      },
+      dist: {
+        files: {'js/compiled.min.js': ['<%= concat.js.dest %>']}
+      }
+    },
+
+    watch: {
+      options: {
+        livereload: true,
+        nospaces: true
+      },
+      scripts: {
+        files: ['<%= concat.js.src %>'],
+        tasks: ['concat','uglify'],
+      },
+      less: {
+        files: ['less/*.less'],
+        tasks: ['less','postcss']
+      }
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-postcss');
+  grunt.registerTask('default', ['watch']);
+};
---
+++ js/marathonlviv.js
@@ -0,0 +1,39 @@
+var clock;
+$(document).ready(function() {
+  // **
+  // * Header for full page
+  // **
+  $(window).resize(function() {
+      $('header.front-page').css("min-height", function() {
+          return $(window).height();
+      });
+  });
+  $(window).trigger('resize');
+
+  // **
+  // * Setting flipclock.js
+  // **
+  // Set dates.
+  var futureDate  = new Date("April 24, 2016 02:00 AM EDT");
+  var currentDate = new Date();
+  // Calculate the difference in seconds between the future and current date
+  var diff = futureDate.getTime() / 1000 - currentDate.getTime() / 1000;
+  // Calculate day difference and apply class to .clock for extra digit styling.
+  function dayDiff(first, second) {
+    return (second-first)/(1000*60*60*24);
+  }
+  if (dayDiff(currentDate, futureDate) < 100) {
+    $('.clock').addClass('twoDayDigits');
+  } else {
+    $('.clock').addClass('threeDayDigits');
+  }
+  if(diff < 0) {
+    diff = 0;
+  }
+  // Instantiate a coutdown FlipClock
+  clock = $('.clock').FlipClock(diff, {
+    clockFace: 'DailyCounter',
+    language: 'ua',
+    countdown: true
+  });
+});
