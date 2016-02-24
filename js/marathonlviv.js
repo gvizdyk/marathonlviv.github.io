bd5dd37036a35fc826c13136a8e618108946975f
Yuriy Hvozdovych <gvizdyk@gmail.com>
2016-02-24 17:28:58 +0200

    reincluded Google Analytics

--- js/marathonlviv.js
+++ js/marathonlviv.js
@@ -45,7 +45,8 @@ $(document).ready(function() {
     nav:true,
     responsive:{
       0:{
-          items:1
+          items:1,
+          nav:false
       },
       480:{
           items:2
@@ -58,15 +59,5 @@ $(document).ready(function() {
       }
     }
   });
-
-  // **
-  // * Google Analytics
-  // **
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-  ga('create', 'UA-65285012-2', 'auto');
-  ga('send', 'pageview');
 
 });
